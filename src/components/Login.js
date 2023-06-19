3d8c2c61ab457c658491101e67552be7ab98464a
saumya9889 <msaumya114@gmail.com>
2023-06-19 16:57:01 +0530

    updated code

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import  "../assests/css/Login.css";
-import  Mask from "..//assests/Mask-group.png"
+import "../assests/css/Login.css";
+import Mask from "..//assests/Mask-group.png"
 import Woman from "../assests/young-woman.png"
-import Meditation from "../assests/meditation.png" 
+import Meditation from "../assests/meditation.png"
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';const Login=()=>{
-	return(
-       <>
-       <div className='login'>
-	<div className='login-left'>
-    {/* <div className='login-item-wrap'>
+import { Carousel } from 'react-responsive-carousel'; 
+const Login = () => {
+    return (
+        <>
+            <div className='login'>
+                <div className='login-left'>
+                    {/* <div className='login-item-wrap'>
     <div className='skip-item'>
     <h6>skip</h6>
             
@@ -19,70 +20,79 @@ import { Carousel } from 'react-responsive-carousel';const Login=()=>{
             
             </div>
             </div> */}
-            <div className='logo-content'>
-            <div>
-		<h1>YOGACURES</h1>
-        </div>
-        <p> Vivekanad Yoga <br/> Anusandhana Samasthana</p>
-        </div>
+                    <div className='logo-content'>
+                        <div>
+                            <h1>YOGACURES</h1>
+                        </div>
+                        <p> Vivekanad Yoga <br /> Anusandhana Samasthana</p>
+                    </div>
 
-        <h2>Welcome Back!</h2>
-        <p className='p2'>Log in to your existant account</p>
-        <div className='login-form'>
-        <form>
-        <div className='text1'>
-            <label>
-               <p>Email/Phone Number</p>
-                {/* <Field type="text" name="username" />
+                    <h2>Welcome Back!</h2>
+                    <p className='p2'>Log in to your existant account</p>
+                    <div className='login-form'>
+                        <form>
+                            <div className='text1'>
+                                <label>
+                                    <p>Email/Phone Number</p>
+                                    {/* <Field type="text" name="username" />
             <ErrorMessage name="username" /> */}
-            </label>
-            <input type="text" name="email" id="email/phone number" placeholder='Type your email'/>
-            <i className="fa-solid fa-envelope"></i>
+                                </label>
+                                <input type="text" name="email" id="email/phone number" placeholder='Type your email' />
+                                <i className="fa-solid fa-envelope"></i>
+                            </div>
+                            <div className='text2'>
+                                <label>
+                                    <p>Password: </p>
+                                </label>
+                                <input type="password" name="password" id="password" placeholder='Password' />
+                                <i class="fa-solid fa-lock"></i>
+                                <i class="fas fa-eye"></i>
+                            </div>
+                            <button type="submit">Login</button>
+
+                            <h6 className='forgot'>Forgot Password?</h6>
+                            <p className='comment'> Or you can login with </p>
+
+                        </form>
+                        <button className='google1' type="submit"><i class="fa-brands fa-google"></i>  Google</button>
+
+                        <div className='register'> Don't have you account? <b> Register Now!</b></div>
+
+                    </div>
+                </div>
+
+                {/* log in right page */}
+                <div className='login-right'>
+                    <Carousel  className='cara'
+                    autoPlay={true}
+                    showArrows={false}
+                    showStatus={false}
+                    showThumbs={false}
+                    // showIndicators={false}
+                    swipeable={true}
+                    transitionTime={1000}>
+                        <div>
+                            <img src={Mask} alt='pic' />
+                            <div className="legend">
+                            <h1>Transform Your Lifestyle, Achieve True Healing : <br/> 
+                                It's More Than Just Medication  </h1>
+                                <p>True healing involves more than just medicating symptoms - it requires modifying <br/> your lifestyle, including your diet and exercise habits, to fully address the underlying <br/> ailment.</p>
+                            </div>
+                        </div>
+                        <figure>
+                            <img src={Woman} alt='pic' />
+                        </figure>
+                        <figure>
+                            <img src={Meditation} alt='pic' />
+                        </figure>
+                    </Carousel>
+
+
+                </div>
+
             </div>
-            <div className='text2'>
-            <label>
-                <p>Password: </p>   
-            </label>
-            <input type="password" name="password" id="password" placeholder='Password' />
-            <i class="fa-solid fa-lock"></i>
-            <i class="fas fa-eye"></i>
-            </div>
-            <button type="submit">Login</button>
-
-            <h6 className='forgot'>Forgot Password?</h6>
-            <p className='comment'> Or you can login with </p>
-
-        </form>
-        <button  className='google1' type="submit"><i class="fa-brands fa-google"></i>  Google</button>
-        
-        <div className='register'> Don't have you account? <b> Register Now!</b></div>
-
-        </div>
-	</div>
-
-    {/* log in right page */}
-    <div className='login-right'>
-    <Carousel  >
-    <div>
-        <img src={Mask} alt='pic'/> 
-         <div>
-         <p className="legend">Legend 1</p>
-         </div>
-    </div>
-    <figure>
-        <img src={Woman} alt='pic'/>
-    </figure>
-    <figure>
-        <img src={Meditation} alt='pic'/>
-    </figure>
-</Carousel>
- 
- 
-    </div>
-    
-    </div>
-    </> 
-	)
+        </>
+    )
 }
 
 export default Login
